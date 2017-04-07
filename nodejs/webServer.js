6afa57458b3a1a494d7876ce9ea951412fa326ae
fodor11 <fodortamas404@gmail.com>
2017-04-07 19:25:26 +0200

    menu items load into content, and smoothscroll to it as well

--- nodejs/webServer.js
+++ nodejs/webServer.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
+// not found
+app.get('/*', function (req, res) {
+    res.end('<h1>Sorry, the requested page could not be found</h1>');
 })
 
 var server = app.listen(8081, function () {
--- webapp/js/angularFunctions.js
+++ webapp/js/angularFunctions.js
@@ -1,3 +1,27 @@
+/// users
+actualUser = "none";
+users = [{ id: 1, email: "li@li.li", password: "su", type: "librarian" },
+         { id: 2, email: "bo@bo.bo", password: "bo", type: "borrower" }];
+
+/// Page ready function
+var LibrarianApp = angular.module('librarianApp', ['smoothScroll']);
+LibrarianApp.controller('librarianController', function ($scope, $http, $location, $anchorScroll) {
+    angular.element(document).ready(function () {
+        var menuBar = document.getElementById('navBarMenu');
+        var menuItems = menuBar.getElementsByTagName('a');
+        for (var i = 0; i < menuItems.length; i++) {
+            menuItems[i].onclick = function (event) {
+                event.preventDefault();
+                $http.get(this.getAttribute('href')).then(function (response) { document.getElementById('content').innerHTML = response.data; });
+                //$location.hash('content');
+                //$anchorScroll();
+            };
+        }
+        $http.get('services.html').then(function (response) { document.getElementById('content').innerHTML = response.data; });
+    });
+});
+
+/// sign in
 var signInApp = angular.module('SignIn', []);
 signInApp.controller('SignInCtrl', function ($scope) {
     $scope.emailAddress = "";
@@ -5,12 +29,18 @@ signInApp.controller('SignInCtrl', function ($scope) {
     $scope.emailExists = 0;
 
     $scope.checkExistingEmail = function () {
-        if ($scope.emailExists) {
-            $scope.emailExists = 0;
-        }
-        else {
-            $scope.emailExists = 1;
-        }
+        $scope.emailExists = 0;
+        // iterate over users
+        angular.forEach(users, function (user, key) {
+            /// iterate over a user's data
+            angular.forEach(user, function (value, key) {
+                if (key == "email") {
+                    if (value == $scope.emailAddress) {
+                        $scope.emailExists = 1;
+                    }
+                }
+            });
+        });
     };
 
     $scope.login = function () {
