a511618ba7c8181ac0c222f5f4ad2b203e75d286
fodor11 <fodortamas404@gmail.com>
2017-04-10 12:53:06 +0200

    server now returns the index.html as homepage and any needed files for it to run

--- nodejs/webServer.js
+++ nodejs/webServer.js
@@ -1,23 +1,40 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
+/// --------------------------------------- ""Database"" --------------------------------------- ///
 var usersArray = {
     users:
     [{ id: 1, name: "Marvelous Librarian", email: "li@li.li", password: "li", type: "librarian", gender: "male", age: "50" },
     { id: 2, name: "Marvelous Borrower", email: "bo@bo.bo", password: "bo", type: "borrower", gender: "female", age: "24" }]
 };
 
+/// --------------------------------------- Homepage --------------------------------------- ///
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../webapp', 'index.html'));
+})
 
+/// --------------------------------------- Get data --------------------------------------- ///
 app.get('/getUsers', function (req, res) {
     res.json(usersArray);
 })
 
+/// --------------------------------------- Add data --------------------------------------- ///
 app.post('/addUser', function (req, res) {
     
 })
 
+/// --------------------------------------- Delete data --------------------------------------- ///
 app.delete('/deleteUser', function (req, res) {
 
+})
+
+/// --------------------------------------- Get files from directories --------------------------------------- ///
+app.get('/:filename', function (req, res) {
+    res.sendFile(path.join(__dirname, '../webapp', req.params.filename));
+})
+app.get('/:dir/:filename', function (req, res) {
+    res.sendFile(path.join(__dirname, '../webapp/' + req.params.dir, req.params.filename));
 })
 
 
